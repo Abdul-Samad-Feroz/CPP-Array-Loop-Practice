da4c2366676d460819c6074250834973e56c5a3f
Abdul Samad <abdulsamadferoz786@gmail.com>
2024-09-21 17:40:03 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // string name = "Abdul Samad";
+
+    string name [10] = {"A.Samad", "Anas", "Bilal", "Sarfaraz Ahmed", "Bobzy the King", "M.Amir"};
+    //cout<< name[4] <<endl;
+
+    for(int i=0; i<15; i++){
+        cout<< name[4] <<endl;
+    }
+
+    return 0;
+}
+
